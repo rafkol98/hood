42a13f6a19d047cad718cb33e928ac2fa5246875
rafaelkoll <rafcall98@hotmail.com>
2020-09-14 19:31:57 +0300

    totalBricks, homepage improvement

---
+++ js/totalBricksChart.js
@@ -0,0 +1,72 @@
+// Count the total bricks that were raised.
+function countTotalBricks() {
+
+    var database = firebase.database();
+    var sumTotal = 0;
+    var bronze, silver, gold;
+
+    var array =[];
+    // var flag = false;
+    for (var i = 1; i <= 3; i++) { 
+        
+        // Find just the last child for each tier.
+        database.ref(`Logistics/History/Pool${i}`).limitToLast(1).once('value').then(function (snapshot) {
+            snapshot.forEach(function (child) {
+                var bricksSpread = child.child("moneySpread").val();
+
+                array.push(bricksSpread);
+                console.log(array);
+                sumTotal = sumTotal + bricksSpread;
+                console.log("sumTotal" + sumTotal);
+                $("#bricksTotal").html(sumTotal);
+                
+            });
+
+            if(array.length!=0){
+                console.log("in");
+        
+            var ctx = document.getElementById('totalBricksCanvas').getContext('2d');
+            var chart = new Chart(ctx, {
+                // The type of chart we want to create
+                type: 'doughnut',
+            
+                // The data for our dataset
+                data: {
+                    labels: ['BRONZE', 'SILVER', 'GOLD'],
+                    datasets: [{
+                        label: 'People',
+                        backgroundColor: ['#CD7F32','#C0C0C0','#D4AF37'],
+                        borderColor: 'black',
+                        data: array
+                    }]
+                },
+            
+                // Configuration options go here
+                options: {
+                    title:{
+                        display:false,
+                        text:'',
+                        fontColor: 'white',
+                        fontSize:30
+            
+                    },
+                    legend:{
+                        display:true,
+                        position:'bottom',
+                        
+                    },
+                }
+            });
+            
+        
+            }
+        });
+    }
+
+    
+
+
+
+
+}
+
